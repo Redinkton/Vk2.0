166e9c9e9e4ad0bd26fa8f6819a028582ef885ec
Redinkton <104201122+Redinkton@users.noreply.github.com>
2023-02-15 10:03:39 +0300

    Add files via upload

---
+++ Infrastructure/Log/Logger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Log
+{
+    /// <summary>
+    /// Класс логгирования ошибок в файл.
+    /// </summary>
+    public static class Logger
+    {
+        private static BlockingCollection<string> _blockingCollection;
+        private static string _filename = "log.txt";
+        private static Task _task;
+
+        static Logger()
+        {
+            _blockingCollection = new BlockingCollection<string>();
+
+            _task = Task.Factory.StartNew(() =>
+            {
+                using (var streamWriter = new StreamWriter(_filename, true, Encoding.UTF8))
+                {
+                    streamWriter.AutoFlush = true;
+
+                    foreach (var s in _blockingCollection.GetConsumingEnumerable())
+                        streamWriter.WriteLine(s);
+                }
+            },
+            TaskCreationOptions.LongRunning);
+        }
+
+        public static void WriteLog(int errorCode, string errorDiscription)
+        {
+            _blockingCollection.Add($"{DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss.fff")} код: {errorCode.ToString()}, описание: {errorDiscription} ");
+        }
+
+        public static void Flush()
+        {
+            _blockingCollection.CompleteAdding();
+            _task.Wait();
+        }
+    }
+}
